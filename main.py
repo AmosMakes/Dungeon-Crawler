92a234ee7a3a68bb7c0cd2c465455d9e030897db
Lucas <100138484+LucasLUCX@users.noreply.github.com>
2024-09-02 19:09:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,4 @@ if __name__ == '__main__':
     
 #----------------------------------------------------------
 #| Hello thanks for looking over my simple python project |
-#| this was done so i can refresh my pyhton skils for this|
-#| project i used OOP way of programing maybe i will      |
-#| with FP programing idk                                 |
-#| Originaly i wanted to have one big .py class file i    |
-#| split them because i think this is more scalabel way   |
-#| i know this isnt big project but still is something    |
-#| recources.py for conviniece purpose this wasnt made to |
-#| this code isnt perfect many issues but i couldnt be    |
-#| bothered to make it something realy good               |
-#|                   :> -Lucas                            |
 #----------------------------------------------------------
